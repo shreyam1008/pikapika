4a79f26f63fdf5665a12931d5493e4469ca4431b
Shreyam Adhikari <shreyam1008@gmail.com>
2021-05-02 10:16:22 +0545

    api initilizer

--- src/view/Body.jsx
+++ src/view/Body.jsx
@@ -1,9 +1,26 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 const Body = () => {
+  //use redux store
+  const [pokemonCount, setPokemonCount] = useState(0);
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((response) => {
+      setPokemonCount(response.data.count);
+      setPokemonList(response.data.results);
+    });
+  }, []);
+
   return (
     <div>
-      <h2>main body</h2>
+      <h2>
+        Total Pokemon List:
+        {pokemonCount}
+      </h2>
+      {pokemonList.map((pokemon) => (
+        <h4>{pokemon.name}</h4>
+      ))}
     </div>
   );
 };
